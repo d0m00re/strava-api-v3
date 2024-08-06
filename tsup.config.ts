9017dae68ac504f4ee4c9c3b808a0eee874f2e09
d0m00re <helsonalexis@gmx.com>
2024-08-06 23:06:45 +0200

    fix esm module

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'tsup';
  
 export default defineConfig({
-    format: ['cjs', 'esm'],
+    format: ['esm'],
     entry: ['./src/index.ts'],
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
     clean: true,
+    target: 'esnext', // Ensure the target is set to a modern version of ECMAScript
 });
